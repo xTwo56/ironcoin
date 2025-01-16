4281144d3aab49af563079cdcfda179341eda10a
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-16 19:08:42 +0530

    feat: ix deposit added

--- programs/rockcoin/src/lib.rs
+++ programs/rockcoin/src/lib.rs
@@ -1,16 +1,23 @@
 use anchor_lang::prelude::*;
 
+use constants::*;
+use instructions::*;
+use state::*;
+mod constants;
+mod instructions;
+mod state;
+
 declare_id!("Bqvu7yb2NgXhThMrYapQiCgRAgtRVUd9VfQ2uq1JebLE");
 
 #[program]
 pub mod rockcoin {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_config(ctx: Context<InitializeConfig>) -> Result<()> {
+        process_initialize_config(ctx)
+    }
+
+    pub fn update_config(ctx: Context<UpdateConfig>, min_health_factor: u64) -> Result<()> {
+        process_update_config(ctx, min_health_factor)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
