4c3800af4f244e9d04c7f82f89fe521f94bb6c7a
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-16 19:14:34 +0530

    modified lib.rs adding deposit ix

--- programs/rockcoin/src/lib.rs
+++ programs/rockcoin/src/lib.rs
@@ -20,4 +20,11 @@ pub mod rockcoin {
     pub fn update_config(ctx: Context<UpdateConfig>, min_health_factor: u64) -> Result<()> {
         process_update_config(ctx, min_health_factor)
     }
+    pub fn deposit_collateral_and_mint(
+        ctx: Context<DepositCollateralAndMintTokens>,
+        amount_collateral: u64,
+        amount_to_mint: u64,
+    ) -> Result<()> {
+        process_deposit_collateral_and_mint_tokens(ctx, amount_collateral, amount_to_mint)
+    }
 }
